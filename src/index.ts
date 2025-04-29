ebe23bae206e9678c1bd745decacc5c6e7ee1f86
Daniel Laskewitz <daniel.laskewitz@microsoft.com>
2025-04-29 15:44:22 +0200

    fixed mcpserver name

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
 
 const server = new McpServer({
-  name: "jokesSSE",
+  name: "jokesMCP",
   description: "A server that provides jokes",
   version: "1.0.0",
   tools: [
